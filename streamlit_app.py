8247b0ac58fcbe158e6ace056f00397ee2ff8bd2
pawan pande <pawanppande1496@gmail.com>
2023-08-09 18:45:08 +0530

    Update streamlit_app.py
    Adding the subheader and text into the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.subheader('Breakfast menu')
+
+streamlit.text('omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard boiled free-range egg')
