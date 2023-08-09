0dc35e21a924377ff7747da25c3531a40170d1ff
pawan pande <pawanppande1496@gmail.com>
2023-08-09 18:47:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.subheader('Breakfast menu')
 
-streamlit.text('omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & Rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞Avacado Toast')
+   
